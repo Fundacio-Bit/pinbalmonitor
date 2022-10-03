068a8c39f672303063eec9000de421c558edec07
bverges-fundaciobit <bverges@fundaciobit.org>
2022-10-03 12:56:08 +0200

    comentaris al codi

--- pinbalmonitor-back/codi_react/src/components/serveis/Servei.js
+++ pinbalmonitor-back/codi_react/src/components/serveis/Servei.js
@@ -5,21 +5,24 @@ import "./Servei.css";
 
 export default function Servei(props) {
   let titolServei = props.servei.titolServei;
-
-  /* Renderitzar totes les peticions que pertanyen servei 
-  (la info s'agafa de serveis/data/dadesSeveis) i és pasada per props
-  des de l'enrutador a través del component pare (layout):
-  */
-  let peticions = props.servei.peticions;
-  const peticionsElement = peticions.map((peticio, index) => (
-    <Peticio key={index} {...peticio} />
-  ));
-
+  /* La variable d'entorn serveix per ser passada al component
+   ** fill per a que la petició apunti a la url corresponent.
+   ** L'entorn és modificat a través del component Opcions entorn
+   ** que accedeix al handlechangei i a l'entorn a través de props.
+   */
   const [entorn, setEntorn] = useState("proves");
   function handleChange(e) {
     setEntorn(e.target.value);
-
   }
+
+  /* Renderitzar totes les peticions que pertanyen servei
+   **(la info s'agafa de serveis/data/dadesSeveis) i és pasada per props
+   ** des de l'enrutador a través del component pare (layout):
+   */
+  let peticions = props.servei.peticions;
+  const peticionsElement = peticions.map((peticio, index) => (
+    <Peticio entorn={entorn} key={index} {...peticio} />
+  ));
 
   return (
     <div className="servei">
