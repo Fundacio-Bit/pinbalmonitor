8115f3aef6b61a973686e70b84da378c334f2c01
bverges-fundaciobit <bverges@fundaciobit.org>
2022-10-03 13:00:42 +0200

    afegida variable entorn en Petició

--- pinbalmonitor-back/codi_react/src/components/peticio/Peticio.js
+++ pinbalmonitor-back/codi_react/src/components/peticio/Peticio.js
@@ -3,8 +3,9 @@ import "./Peticio.css";
 
 export default function Peticio(props) {
   let nom = props.nom;
-
- 
+  let entorn = props.entorn 
+ // entorn -> variable que servirà per saber quina petició es cridarà 
+  // en el controlador: la de producció o la de proves
   return (
     <div className="container-peticio">
       <span>{nom}</span>
