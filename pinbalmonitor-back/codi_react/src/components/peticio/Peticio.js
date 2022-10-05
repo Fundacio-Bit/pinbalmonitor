d1e328e9febb7b6b39bcc520d14cac1556ae3a0f
bverges-fundaciobit <bverges@fundaciobit.org>
2022-10-05 12:08:27 +0200

    Fets estils del component petició #6

--- pinbalmonitor-back/codi_react/src/components/peticio/Peticio.js
+++ pinbalmonitor-back/codi_react/src/components/peticio/Peticio.js
@@ -1,19 +1,79 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import "./Peticio.css";
-import  Button  from "@mui/material/Button";
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import ReplayIcon from "@mui/icons-material/Replay";
+import { CircularProgress, Tooltip } from "@mui/material";
 
 export default function Peticio(props) {
+  const [loading, setLoading] = useState(false);
+  const [resultatProva, setResultatProva] = useState("");
+
+  /** Loading fals temporal per a poder fer els estils  de la petició quan carrega
+   * TODO: Ha de ser esborrat quan tinguem el controlador.
+   */
+  function mockLoading() {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      /* Temporalment
+      Per fer sa prova d'una petició exitosa es fa a proves,
+       per fer sa prova d'un error es fa a producció"*/
+      entorn === "proves" ? setResultatProva("exit") : setResultatProva("fall");
+    }, 2000);
+  }
+
+  function iconePlayOReplay() {
+    return resultatProva === "" ? (
+      <PlayArrowIcon className="executar-icone"></PlayArrowIcon>
+    ) : (
+      <ReplayIcon className="executar-icone"></ReplayIcon>
+    );
+  }
+
   let nom = props.nom;
   let entorn = props.entorn;
   // entorn -> variable que servirà per saber quina petició es cridarà
   // en el controlador: la de producció o la de proves
+
+  useEffect(() => {
+    setResultatProva("");
+  }, [entorn]);
+
   return (
-    <div className="container-peticio">
-      <span>{nom}</span>
-      <Button variant="contained" size="medium" endIcon={<PlayArrowIcon />}>
-      Executar
-      </Button>
-    </div>
+    <Tooltip
+      arrow
+      placement="left-start"
+      title={`${loading ? "Executant..." : "Executar"}`}
+    >
+      <div
+        onClick={mockLoading}
+        className={`container-peticio ${loading ? "loading" : ""} ${
+          resultatProva === "exit" ? "exit" : ""
+        } ${resultatProva === "fall" ? "fall" : ""}`}
+      >
+        <div className="nombre-peticio-container">
+          <span>{props.nombre}</span>
+        </div>
+        {!loading ? (
+          <div className="text-peticio">
+            <span className="nom-peticio">{nom}</span>
+          </div>
+        ) : (
+          ""
+        )}
+        <div className="executar-boto">
+          {
+            // Si està carregant tindrem loader, si no, botó de play
+            loading ? (
+              <CircularProgress className="loader" />
+            ) : (
+              iconePlayOReplay()
+            )
+          }
+        </div>
+      </div>
+      
+    </Tooltip>
+    
   );
 }
