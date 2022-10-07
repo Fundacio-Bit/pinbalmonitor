c7795f014256b56db1894225caf36a56dc158214
bverges-fundaciobit <bverges@fundaciobit.org>
2022-10-07 14:26:32 +0200

    #7 fet component de NavBar amb menú integrat

--- pinbalmonitor-back/codi_react/src/components/Layout.js
+++ pinbalmonitor-back/codi_react/src/components/Layout.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Menu from "./menu/Menu";
 import Servei from "./serveis/Servei";
 import "./Layout.css";
+import Navbar from "./navbar/Navbar";
 
 
 export default function Layout(props) {
   return (
     <div className="layout">
-      <Menu />
+    <Navbar/>
       <br />
       <Servei servei={props.servei} />
     </div>
