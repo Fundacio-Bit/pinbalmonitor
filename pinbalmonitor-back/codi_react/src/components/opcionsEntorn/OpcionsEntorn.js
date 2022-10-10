ca41ab4f14833f6b4359467542bf9f8643a4a121
bverges-fundaciobit <bverges@fundaciobit.org>
2022-10-10 10:39:21 +0200

    Estil de selecció d'entorn #22

--- pinbalmonitor-back/codi_react/src/components/opcionsEntorn/OpcionsEntorn.js
+++ pinbalmonitor-back/codi_react/src/components/opcionsEntorn/OpcionsEntorn.js
@@ -1,14 +1,72 @@
 import React from "react";
+import {
+  Radio,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+} from "@mui/material";
 
 export default function OpcionsEntorn(props) {
-  const {handleChange,entorn} =props
+  const { handleChange, entorn } = props;
 
+  const radioStyles = {
+    "&.Mui-checked": {
+      color: "var(--color-primari)",
+    },
+  };
 
+  const labelStyles = {
+    "&.Mui-focused": {
+      color: "var(--color-primari)",
+    },
+  };
 
   return (
-    <div onChange={() =>handleChange}>
-      <input type="radio" checked={entorn === 'proves'} value="proves" name="entorn" onChange={handleChange} /> Proves
-      <input type="radio"  checked={entorn === 'produccio'} value="produccio" name="entorn"  onChange={handleChange} /> Producció
+    <div onChange={() => handleChange}>
+      <FormControl>
+        <FormLabel id="demo-controlled-radio-buttons-group" sx={labelStyles}>
+          Seleccioni l'entorn
+        </FormLabel>
+        <RadioGroup
+          row
+          className="radio-group"
+          aria-labelledby="demo-controlled-radio-buttons-group"
+          name="controlled-radio-buttons-group"
+          onChange={handleChange}
+        >
+          <FormControlLabel
+            value="proves"
+            control={
+              <Radio
+                sx={radioStyles}
+                type="radio"
+                checked={entorn === "proves"}
+                value="proves"
+                name="entorn"
+                size="small"
+                onChange={handleChange}
+              />
+            }
+            label="Proves"
+          />
+          <FormControlLabel
+            value="producio"
+            control={
+              <Radio
+                sx={radioStyles}
+                type="radio"
+                checked={entorn === "produccio"}
+                value="produccio"
+                name="entorn"
+                size="small"
+                onChange={handleChange}
+              />
+            }
+            label="Produccio"
+          />
+        </RadioGroup>
+      </FormControl>
     </div>
   );
 }
