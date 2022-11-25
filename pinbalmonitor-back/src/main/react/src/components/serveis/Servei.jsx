84d96ed39a4bcfa6d294d76bcb7a9062b71f9164
bverges-fundaciobit <bverges@fundaciobit.org>
2022-11-25 08:40:06 +0100

    #51 canvis en el component Servei per deixar el projecte funcional

--- pinbalmonitor-back/src/main/react/src/components/serveis/Servei.jsx
+++ pinbalmonitor-back/src/main/react/src/components/serveis/Servei.jsx
@@ -1,22 +1,22 @@
-import { React, useState } from 'react';
-import PropTypes from 'prop-types';
-import OpcionsEntorn from '../opcionsEntorn/OpcionsEntorn';
-import Peticio from '../peticio/Peticio';
-import functions from '../helpers';
-import './Servei.css';
+/* eslint-disable */ 
+import  React, { useState } from "react";
+import OpcionsEntorn from "../opcionsEntorn/OpcionsEntorn";
+import Peticio from "../peticio/Peticio";
+import functions from "../helpers";
+import "./Servei.css";
 
 export default function Servei(props) {
-  const { servei } = props;
-  const { titolServei } = servei;
+  let servei = props.servei;
+  let titolServei = servei.titolServei;
   /* La variable d'entorn serveix per ser passada al component
    ** fill per a que la petició apunti a la url corresponent.
    ** L'entorn és modificat a través del component Opcions entorn
    ** que accedeix al handlechangei i a l'entorn a través de props.
    */
-  const [entorn, setEntorn] = useState('proves');
-  const handleChange = (e) => {
+  const [entorn, setEntorn] = useState("proves");
+  function handleChange(e) {
     setEntorn(e.target.value);
-  };
+  }
 
   /* Renderitzar totes les peticions que pertanyen servei
    **(la info s'agafa de serveis/data/dadesSeveis) i és pasada per props
@@ -24,54 +24,50 @@ export default function Servei(props) {
    */
   function renderitzarLlistaDePeticions(
     llistaPeticions,
-    subservei, /** paràmetre opcional */
+    subservei /** paràmetre opcional */
   ) {
     return llistaPeticions.map((peticio, index) => (
       <Peticio
         entorn={entorn}
-        id={`${peticio.nom}${subservei ? (` ${subservei.nom}`) : ''} ${entorn}`}
+        id={`${peticio.nom}${subservei ? (" " + subservei.nom) : ""} ${entorn}`}
         key={`${peticio.nom} ${entorn}`}
         nombre={index + 1}
-        nom={peticio.nom}
+        {...peticio}
       />
     ));
   }
   function renderitzarServeiSimple() {
-    const { peticions } = servei;
+    let peticions = servei.peticions;
     return (
       <ul className="peticions">{renderitzarLlistaDePeticions(peticions)}</ul>
     );
   }
 
   function renderitzarServeiAmbSubserveis() {
-    const { subserveis } = servei;
-    return subserveis.map((subservei) => (
-      <div className="subservei" key={subservei.nom}>
-        <h2>
-          {' '}
-          {subservei.nom}
-        </h2>
+    let subserveis = servei.subserveis;
+    return subserveis.map((subservei, index) => (
+      <div className="subservei" key={index}>
+        <h2> {subservei.nom}</h2>
         <ul className="peticions">
           {renderitzarLlistaDePeticions(subservei.peticions, subservei)}
         </ul>
       </div>
     ));
   }
-  const renderitzarServei = () => (servei.subserveis
+  const renderitzarServei = () => {
+    return servei.subserveis
       ? renderitzarServeiAmbSubserveis()
-      : renderitzarServeiSimple());
+      : renderitzarServeiSimple();
+  };
 
   return (
     <div className="servei">
       <div className="superior">
         <h1>
-          {titolServei}
-          {' '}
-          -
-          {' '}
+          {titolServei} -{" "}
           <span
             className={`text-entorn ${
-              entorn === 'proves' ? 'text-proves' : 'text-producció'
+              entorn === "proves" ? "text-proves" : "text-producció"
             }`}
           >
             {functions.capitalitzar(entorn)}
@@ -83,18 +79,3 @@ export default function Servei(props) {
     </div>
   );
 }
-
-Servei.propTypes = {
-  servei: PropTypes.oneOfType([PropTypes.shape({
-    titolServei: PropTypes.string.isRequired,
-    peticions: PropTypes.arrayOf(PropTypes.obj).isRequired,
-  }),
-    PropTypes.shape({
-      titolServei: PropTypes.string.isRequired,
-      subserveis: PropTypes.arrayOf(PropTypes.shape({
-        nom: PropTypes.string.isRequired,
-        peticions: PropTypes.arrayOf(PropTypes.obj).isRequired,
-      })),
-     })]).isRequired,
-
-};
