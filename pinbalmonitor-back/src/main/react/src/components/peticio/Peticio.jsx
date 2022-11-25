0c8eaa4c7f3ead502538d1d51188960797f63bb4
bverges-fundaciobit <bverges@fundaciobit.org>
2022-11-25 08:41:28 +0100

    canvis en component petició per a deixar el projecte funcional #49

--- pinbalmonitor-back/src/main/react/src/components/peticio/Peticio.jsx
+++ pinbalmonitor-back/src/main/react/src/components/peticio/Peticio.jsx
@@ -1,57 +1,48 @@
-import { React, useState } from 'react';
-import { useSessionStorage } from 'react-use';
-import './Peticio.css';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import ReplayIcon from '@mui/icons-material/Replay';
-import { CircularProgress, Tooltip } from '@mui/material';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import ErrorIcon from '@mui/icons-material/Error';
-import PropTypes from 'prop-types';
-import SnackbarResultat from '../snackbarResultat/SnackbarResultat';
-import functions from '../helpers';
-
+/* eslint-disable */ 
+import  React, { useState } from "react";
+import { useSessionStorage } from "react-use";
+import "./Peticio.css";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import ReplayIcon from "@mui/icons-material/Replay";
+import { CircularProgress, Tooltip } from "@mui/material";
+import SnackbarResultat from "../snackbarResultat/SnackbarResultat";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ErrorIcon from "@mui/icons-material/Error";
+import functions from "../helpers";
 export default function Peticio(props) {
-  const {
- id, nom, entorn, nombre,
-} = props;
   const [loading, setLoading] = useState(false);
   const [resultatProva, setResultatProva] = useSessionStorage(
-    `resultat ${id}`,
-    '',
+    `resultat ${props.id}`, ""
   );
   const [obrirSnackbar, setObrirSnackbar] = useState(false);
   const [ultimaProva, setUltimaProva] = useSessionStorage(
-    `ultimaProva ${id}`,
-    '',
+    `ultimaProva ${props.id}`,
+    ""
   );
 
   /** Loading fals temporal per a poder fer els estils  de la petició quan carrega
    * TODO: Ha de ser esborrat quan tinguem el controlador.
    */
-
-   function calcularResultatProva() {
-    return Math.random() < 0.5
-      ? setResultatProva('exit')
-      : setResultatProva('fall');
-  }
-
   function mockLoading() {
-    // fetch temporal per simular request -> resultat aleatori
+    //fetch temporal per simular request -> resultat aleatori
     setLoading(true);
-    fetch('https://reqres.in/api/users?delay=3')
+    fetch("https://reqres.in/api/users?delay=3")
       .then((res) => {
         console.log(res);
         setLoading(false);
-        calcularResultatProva();
+        Math.random() < 0.5
+          ? setResultatProva("exit")
+          : setResultatProva("fall");
         setUltimaProva(
-          ` Última execució: ${new Date().toLocaleString('ca-ES', {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit',
-          })}`,
+          " Última execució: " +
+            new Date().toLocaleString("ca-ES", {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+            })
         );
       })
       .then(() => {
@@ -59,83 +50,68 @@ export default function Peticio(props) {
       })
       .catch((err) => {
         console.error(`Error: ${err.message}`);
-        setResultatProva('');
+        setResultatProva("");
         setLoading(false);
       });
   }
 
   function iconePlayOReplay() {
-    return resultatProva === '' ? (
-      <PlayArrowIcon className="executar-icone" />
+    return resultatProva === "" ? (
+      <PlayArrowIcon className="executar-icone"></PlayArrowIcon>
     ) : (
-      <ReplayIcon className="executar-icone" />
+      <ReplayIcon className="executar-icone"></ReplayIcon>
     );
   }
 
   function iconeResultat() {
-    if (resultatProva && !loading) {
-      return resultatProva === 'exit' ? (
+    if (resultatProva && !loading)
+      return resultatProva === "exit" ? (
         <CheckCircleIcon className="icone-output-exit icone-output">
-          {' '}
+          {" "}
         </CheckCircleIcon>
       ) : (
-        <ErrorIcon className="icone-output-fall icone-output" />
+        <ErrorIcon className="icone-output-fall icone-output"></ErrorIcon>
       );
-    }
-    return {};
   }
 
+  let nom = props.nom;
+  let entorn = props.entorn;
+  // entorn -> variable que servirà per saber quina petició es cridarà
+  // en el controlador: la de producció o la de proves
+
   return (
-    <li className="container-peticio" aria-label={id}>
+    <li className="container-peticio" aria-label={props.id}>
       <Tooltip
         arrow
         placement="left-start"
-        title={`${loading ? 'Executant...' : 'Executar'}`}
+        title={`${loading ? "Executant..." : "Executar"}`}
       >
         <button
-          type="button"
           onClick={mockLoading}
-          className={`peticio ${loading ? 'loading' : ''} ${
-            resultatProva === 'exit' ? 'exit' : ''
-          } ${resultatProva === 'fall' ? 'fall' : ''}`}
+          className={`peticio ${loading ? "loading" : ""} ${
+            resultatProva === "exit" ? "exit" : ""
+          } ${resultatProva === "fall" ? "fall" : ""}`}
         >
           <div className="nombre-peticio-container">
-            <span>
-              {nombre}
-              {' '}
-            </span>
+            <span>{props.nombre} </span>
           </div>
           {!loading ? (
             <div className="text-peticio">
-              <span className="nom-peticio">
-                {nom}
-                {' '}
-                (
-                {functions.capitalitzar(entorn)}
-                )
-              </span>
+              <span className="nom-peticio">{nom} ({functions.capitalitzar(entorn)})</span>
               {ultimaProva ? (
-                <span className="timestamp">
-                  {' '}
-                  (
-                  {' '}
-                  {ultimaProva}
-                  {' '}
-                  )
-                  {' '}
-                </span>
+                <span className="timestamp"> ( {ultimaProva} ) </span>
               ) : (
-                ''
+                ""
               )}
             </div>
           ) : (
-            ''
+            ""
           )}
           <div className="executar-boto">
             {
               // Si està carregant tindrem loader, si no, botó de play
               loading ? (
-                <CircularProgress style={{ color: 'var(--blanc)' }} />
+                <CircularProgress style={{ color: "var(--blanc)" }} />
               ) : (
                 iconePlayOReplay()
               )
@@ -145,19 +121,12 @@ export default function Peticio(props) {
         </button>
       </Tooltip>
       <SnackbarResultat
-        obrir={obrirSnackbar}
+        obert={obrirSnackbar}
         resultatProva={resultatProva}
         nom={nom}
         entorn={entorn}
         loading={loading}
-      />
+      ></SnackbarResultat>
     </li>
   );
 }
-
-Peticio.propTypes = {
-  id: PropTypes.string.isRequired,
-  entorn: PropTypes.oneOf(['proves', 'produccio']).isRequired,
-  nom: PropTypes.string.isRequired,
-  nombre: PropTypes.number.isRequired,
-};
