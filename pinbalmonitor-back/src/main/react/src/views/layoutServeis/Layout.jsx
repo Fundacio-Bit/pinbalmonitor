d936b4f67fd928c8f4428e4eddff5e5d8bbcb256
bverges-fundaciobit <bverges@fundaciobit.org>
2022-11-25 08:44:14 +0100

    Canvis per a deixar el projecte funcional en l'arxiu de Layout #52

--- pinbalmonitor-back/src/main/react/src/views/layoutServeis/Layout.jsx
+++ pinbalmonitor-back/src/main/react/src/views/layoutServeis/Layout.jsx
@@ -1,18 +1,14 @@
+/* eslint-disable */ 
 import React from 'react';
 import Proptypes from 'prop-types';
-import BotoScrollup from '../../components/botoScrollup/BotoScrollup';
-import Navbar from '../../components/navbar/Navbar';
 import Servei from '../../components/serveis/Servei';
 import './Layout.css';
-
 export default function Layout(props) {
   const { servei } = props;
   return (
     <div className="layout">
-      <Navbar />
       <br />
       <Servei servei={servei} />
-      <BotoScrollup />
     </div>
   );
 }
