66bcf0602c6060d095edb241cafb3b8d0a143d32
bverges-fundaciobit <bverges@fundaciobit.org>
2022-11-29 09:51:56 +0100

    Canviar en el CSS i l'html la classe servei per 'procediment' #60

--- pinbalmonitor-back/src/main/react/src/components/procediment/Procediment.jsx
+++ pinbalmonitor-back/src/main/react/src/components/procediment/Procediment.jsx
@@ -61,7 +61,7 @@ export default function Procediment(props) {
   };
 
   return (
-    <div className="servei">
+    <div className="procediment">
       <div className="superior">
         <h1>
           {titol} -{" "}
