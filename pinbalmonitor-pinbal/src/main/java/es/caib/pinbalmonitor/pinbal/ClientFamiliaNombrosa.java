28adb78a7fc7fef7c97a7daae3ae4a1586081575
bverges-fundaciobit <bverges@fundaciobit.org>
2023-03-24 08:50:19 +0100

    #89 fet controlador de família nombrosa

---
+++ pinbalmonitor-pinbal/src/main/java/es/caib/pinbalmonitor/pinbal/ClientFamiliaNombrosa.java
@@ -0,0 +1,41 @@
+package es.caib.pinbalmonitor.pinbal;
+import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
+
+import es.caib.pinbal.client.recobriment.model.ScspJustificante;
+import es.caib.pinbal.client.recobriment.model.ScspRespuesta;
+import es.caib.pinbal.client.recobriment.svdsctfnws01.scdhpa;
+public class ClientFamiliaNombrosa {
+    
+
+    private static final Logger LOG = LoggerFactory.getLogger(ClientFamiliaNombrosa.class);
+
+    @Inject
+    private Configuracio configuracio;
+    private ClientSvdsctfnws01 ClientSvdsctfnws01;
+
+    @PostConstruct
+    protected void init() {
+        LOG.info("Iniciant client");
+        ClientSvdsctfnws01 = new ClientSvdsctfnws01(
+                configuracio.getEndpoint(),
+                configuracio.getUsuari(),
+                configuracio.getSecret());
+        LOG.info("Client creat");
+    }
+
+    public ScspRespuesta peticioSincrona(ClientSvdsctfnws01.SolicitudSvdsctfnws01... solicituds) throws IOException {
+        return ClientSvdsctfnws01.peticionSincrona(List.of(solicituds));
+    }
+
+    public ScspJustificante getJustificant(String idPeticio) throws IOException {
+        return ClientSvdsctfnws01.getJustificante(idPeticio);
+    }
+}
