f7e5bb6a4d2f66d50660df91e79c90db7053837c
bverges-fundaciobit <bverges@fundaciobit.org>
2023-04-19 11:27:00 +0200

    #92 estructura projecte plugins

---
+++ pinbalmonitor-plugins/identitat/src/main/java/es/caib/pinbalmonitor/plugins/identitat/Configuracio.java
@@ -0,0 +1,79 @@
+package es.caib.pinbalmonitor.plugins.identitat;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Configuració de l'exemple de Pinbal.
+ * El definim com a {@link ApplicationScoped} perquè només hi hagi una instància per aplicació.
+ *
+ * @author areus
+ */
+@Named
+@ApplicationScoped
+public class Configuracio {
+
+    // Dades de connexió
+    @Inject
+    @ConfigProperty(name = "es.caib.pinbalmonitor.int.pinbal.endpoint")
+    private String endpoint;
+
+    @Inject
+    @ConfigProperty(name = "es.caib.pinbalmonitor.int.pinbal.usuari")
+    private String usuari;
+
+    @Inject
+    @ConfigProperty(name = "es.caib.pinbalmonitor.int.pinbal.secret")
+    private String secret;
+
+    // Dades de la petició
+
+    @Inject
+    @ConfigProperty(name = "es.caib.pinbalmonitor.pinbal.organismeSolicitant")
+    private String organismeSolicitant;
+
+    @Inject
+    @ConfigProperty(name = "es.caib.pinbalmonitor.pinbal.unitatTramitadora")
+    private String unitatTramitadora;
+
+    @Inject
+    @ConfigProperty(name = "es.caib.pinbalmonitor.pinbal.codiProcediment")
+    private String codiProcediment;
+
+    @Inject
+    @ConfigProperty(name = "es.caib.pinbalmonitor.pinbal.finalitat")
+    private String finalitat;
+
+
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public String getUsuari() {
+        return usuari;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public String getOrganismeSolicitant() {
+        return organismeSolicitant;
+    }
+
+    public String getUnitatTramitadora() {
+        return unitatTramitadora;
+    }
+
+    public String getCodiProcediment() {
+        return codiProcediment;
+    }
+
+    public String getFinalitat() {
+        return finalitat;
+    }
+}
---
+++ pinbalmonitor-plugins/identitat/src/main/java/es/caib/pinbalmonitor/plugins/identitat/TitularModel.java
@@ -0,0 +1,93 @@
+package es.caib.pinbalmonitor.plugins.identitat;
+
+import es.caib.pinbal.client.recobriment.model.ScspTitular;
+import es.caib.pinbal.client.recobriment.model.ScspTitular.ScspTipoDocumentacion;
+import es.caib.pinbalmonitor.plugins.identitat.scsp.TipoDocumentacionSubset;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ * Dades emprades al formulari per indicar les dades a verificar del titular.
+ */
+public class TitularModel implements Serializable {
+
+    public TitularModel( String documentacio,
+            ScspTitular.ScspTipoDocumentacion tipusDocumentacio,
+            String nom, String primerCognom, String segonCognom) {
+        this.documentacio = documentacio;
+        this.tipusDocumentacio = tipusDocumentacio;
+        this.nom = nom;
+        this.primerCognom = primerCognom;
+        this.segonCognom = segonCognom;
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    @NotEmpty
+    @Size(max=9) private String documentacio;
+
+    /**
+     * Tipo de documentació. Aquesta enumeració permet molts de valors, però el servei concret de verificació
+     * d'identitat només permet emprar DNI o NIE, per això validam que sigui un d'aquests valors.
+     */
+    @NotNull
+    @TipoDocumentacionSubset(anyOf = {ScspTitular.ScspTipoDocumentacion.DNI, ScspTitular.ScspTipoDocumentacion.NIE})
+    private ScspTitular.ScspTipoDocumentacion tipusDocumentacio;
+
+    private String nom;
+    private String primerCognom;
+    private String segonCognom;
+
+    public String getDocumentacio() {
+        return documentacio;
+    }
+
+    public void setDocumentacio(String documentacio) {
+        this.documentacio = documentacio;
+    }
+
+    public ScspTitular.ScspTipoDocumentacion getTipusDocumentacio() {
+        return tipusDocumentacio;
+    }
+
+    public void setTipusDocumentacio(ScspTitular.ScspTipoDocumentacion tipusDocumentacio) {
+        this.tipusDocumentacio = tipusDocumentacio;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrimerCognom() {
+        return primerCognom;
+    }
+
+    public void setPrimerCognom(String primerCognom) {
+        this.primerCognom = primerCognom;
+    }
+
+    public String getSegonCognom() {
+        return segonCognom;
+    }
+
+    public void setSegonCognom(String segonCognom) {
+        this.segonCognom = segonCognom;
+    }
+
+    public ScspTitular toScspTitular() {
+        ScspTitular titular = new ScspTitular();
+        titular.setTipoDocumentacion(tipusDocumentacio);
+        titular.setDocumentacion(documentacio);
+        titular.setNombre(nom);
+        titular.setApellido1(primerCognom);
+        titular.setApellido2(segonCognom);
+        return titular;
+    }
+}
